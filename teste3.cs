50666200bd534f073f5e7fac0d7317c225c7f6d3
Jarlison Carvalho - E. Escolinha de TI - BNE <jarlisoncarvalho@employer.matriz>
2024-05-07 09:49:17 -0300

    segundo commit

--- teste3.cs
+++ teste3.cs
@@ -4,18 +4,17 @@ class program
 {
     static void Main(string[] args)
     {
-        int a = 2;
-        int[] v = new int[6];
-
-        while (a < 6)
+        int a = 7;
+        int b = a - 6;
+        int[] v = new int[a];
+        while (b < a)
         {
-            v[a] = 10 * a;
-            a += 1;
-
+            v[b] = b + a;
+            b = b + 2;
         }
-        for (int i = 0; i < 6 ; i++)
+        for(int i = 0; i<a; i++)
         {
-            Console.WriteLine($"v[{i}] = { v[i]}");
+            Console.WriteLine($"v[{i}] = {v[i]}");
 
         }
     }
